a91fe3509421339ddd5ce0f2bdbc37d36e295962
Mudada <mudada@localhost.localdomain>
2016-06-05 18:54:57 +0200

    Initial Commit

---
+++ Midwut.php
@@ -0,0 +1,7 @@
+<?php
+
+require "monster.php";
+
+$opponent = new Monstre;
+
+var_dump($opponent);
---
+++ monster.php
@@ -0,0 +1,17 @@
+<?php
+
+class Monstre {
+
+	protected $lvl = 1;
+	protected $lvl_stats = 1;
+	protected $vie = 100;
+	protected $atk = 10;
+	protected $book= ["Punch", "Headbut"];
+	protected $exp = 10 * $lvl;
+
+}
+
+class Vald extends Monstre {
+	
+
+}
